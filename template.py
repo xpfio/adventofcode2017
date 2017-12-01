fc34bf5aaf75885c10a8935516a1a5f0ccacdff5
Xavier <xpfplantaz@connect.ust.hk>
2017-12-01 20:44:04 +0800

    Update Template

--- template.py
+++ template.py
@@ -1,10 +1,8 @@
 # PART 1
+
 def part1(input):
     """Calculate Part 1 of the problem for any input"""
-    #
-    # code goes here
-    #
-    return 'OUTPUT_'
+    print('AAA')
 
 # PART 2
 def part2(input):
@@ -17,16 +15,12 @@ def part2(input):
 # INPUTS
 
 FILE = 'input/day02.txt'
+
 TESTS_PART1 = [
-    ("INPUT","OUTPUT"),
-    ("INPUT","OUTPUT"),
-    ("INPUT","OUTPUT"),
     ("INPUT","OUTPUT")
 ]
+
 TESTS_PART2 = [
-    ("INPUT","OUTPUT"),
-    ("INPUT","OUTPUT"),
-    ("INPUT","OUTPUT"),
     ("INPUT","OUTPUT")
 ]
 
@@ -35,13 +29,14 @@ TESTS_PART2 = [
 
 ## Part 1 - Tests
 def tests_part1(input, expected_solution):
+    output = str(part1(str(input)))
     """Test Part 1 on the different inputs"""
-    if str(expected_solution) == str(part1(str(input))):
+    if str(expected_solution) == output:
         print('\033[92m' + 'GOOD\tINPUT: ', input, '\tOUTPUT: ', expected_solution, '\033[0m')
     else:
         print('\033[91m' + 'NOPE\t', \
                 'INPUT: ', input, \
-                '\tOUTPUT: ', str(part1(str(input))), \
+                '\tOUTPUT: ', output, \
                 '\tEXPECTED:', expected_solution, \
                 '\033[0m')
 
@@ -62,17 +57,18 @@ print('\n')
 ## Part 2 - Tests
 def tests_part2(input, expected_solution):
     """Test Part 2 on the different inputs"""
-    if str(expected_solution) == str(part2(str(input))):
+    output = str(part2(str(input)))
+    if str(expected_solution) == output:
         print('\033[92m' + 'GOOD\tINPUT: ', input, '\tOUTPUT: ', expected_solution, '\033[0m')
     else:
         print('\033[91m' + 'NOPE\t', \
             'INPUT: ', input, \
-            '\tOUTPUT: ', str(part2(str(input))), \
+            '\tOUTPUT: ', output, \
             '\tEXPECTED', expected_solution, \
             '\033[0m')
 
 for test in TESTS_PART2:
-    tests_part1(test[0], test[1])
+    tests_part2(test[0], test[1])
 
 ## Part 2 - Answer
 print('\033[94m' + 'PART 2 ANSWER: ', str(part2(str(read_input()))))
