011b4a5869a78a360c9b7eb9b8a69f09008fa6b2
Xavier <xpfplantaz@connect.ust.hk>
2017-12-04 13:16:16 +0800

    Day 04

---
+++ day04.py
@@ -0,0 +1,101 @@
+"""Template file for Python source code"""
+import math
+from itertools import groupby
+
+# PART 1
+def part1(s):
+    lines = s.split('\n')
+    count = 0
+    for line in lines:
+        words = line.split(' ')
+        dirty = 0
+        words = sorted(words)
+        b = groupby(words)
+        for a in [len(list(group)) for key, group in b]:
+            if a > 1:
+                dirty = 1
+        if dirty == 0:
+            count += 1
+    return count
+
+def part2(s):
+    lines = s.split('\n')
+    count = 0
+    for line in lines:
+        words = line.split(' ')
+        dirty = 0
+        words = list(map(lambda x: sorted(x),words))
+        words = sorted(words)
+        b = groupby(words)
+        for a in [len(list(group)) for key, group in b]:
+            if a > 1:
+                dirty = 1
+        if dirty == 0:
+            count += 1
+    return count
+
+
+
+
+# INPUTS
+FILE = 'input/day04.txt'
+TESTS_PART1 = [
+    ('aa bb cc dd ee',1),
+    ('aa bb cc dd aa',0),
+    ('aa bb cc dd aaa\naa bb',2),
+    ('aa bb cc dd aaa',1)
+]
+
+TESTS_PART2 = [
+("abcde fghij", 1),
+("abcde xyz ecdab", 0),
+("a ab abc abd abf abj", 1),
+("iiii oiii ooii oooi oooo", 1),
+("oiii ioii iioi iiio", 0)
+]
+
+
+## Part 1 - Tests
+def tests_part1(input, expected_solution):
+    """Test Part 1 on the different inputs"""
+    output = str(part1(str(input)))
+    if str(expected_solution) == output:
+        print('\033[92m' + 'GOOD\tINPUT: ', input, '\tOUTPUT: ', expected_solution, '\033[0m')
+    else:
+        print('\033[91m' + 'NOPE\t', \
+                'INPUT: ', input, \
+                '\tOUTPUT: ', output, \
+                '\tEXPECTED:', expected_solution, \
+                '\033[0m')
+
+for test in TESTS_PART1:
+    tests_part1(test[0], test[1])
+
+# Part 1 - Answer
+def read_input():
+    """Open Daily input file defined in FILE"""
+    with open(FILE) as f:
+        return f.read()
+
+print('\033[94m' + 'PART 1 ANSWER: ', str(part1(str(read_input()))))
+print('\n')
+
+
+## Part 2 - Tests
+def tests_part2(input, expected_solution):
+    """Test Part 2 on the different inputs"""
+    output = str(part2(str(input)))
+    if str(expected_solution) == output:
+        print('\033[92m' + 'GOOD\tINPUT: ', input, '\tOUTPUT: ', expected_solution, '\033[0m')
+    else:
+        print('\033[91m' + 'NOPE\t', \
+            'INPUT: ', input, \
+            '\tOUTPUT: ', output, \
+            '\tEXPECTED', expected_solution, \
+            '\033[0m')
+
+for test in TESTS_PART2:
+    tests_part2(test[0], test[1])
+
+# Part 2 - Answer
+print('\033[94m' + 'PART 2 ANSWER: ', str(part2(str(read_input()))))
--- tools/parse.py
+++ tools/parse.py
@@ -1,8 +1,10 @@
-def fromArrayInt(table):
+def fromArrayInt(table,separator='\t'):
     rows = table.split('\n')
     output = []
     for row in rows:
-        line = row.split('\t')
+        line = row.split(separator)
         line = list(map(lambda x: int(x), line))
         output.append(line)
     return output
+
+# "5	1	9	5\n7	5	3\n2	4	6	8" -> [[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]]
