698ce9dd9e6de51348300465bc916a266b96177a
Xavier <xpfplantaz@connect.ust.hk>
2017-12-08 15:43:34 +0800

    Add Day 08

---
+++ day08.py
@@ -0,0 +1,27 @@
+def day08 (s):
+    # print(s)
+    lines = s.split('\n')
+    registry = {}
+    maximum = 0
+
+    for line in lines:
+        [to_change, up_down, value, if_, from_, test_, value_test] = line.split(' ')
+        if to_change not in registry:
+            registry[to_change] = 0
+        if from_ not in registry:
+            registry[from_] = 0
+        pass_test = eval(str(registry[from_]) + ' ' + test_ + ' ' + value_test)
+
+        if pass_test:
+            direction = 1 if up_down == 'inc' else -1
+            registry[to_change] += int(value) * direction
+
+        
+        maximum = max([maximum] + [registry[a] for a in registry])
+    return maximum
+        
+
+
+with open('input/day08.txt') as f:
+    s = f.read()
+    print(day08(s))
