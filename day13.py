719656d04f016a1f609c5511a3e9a8ede3380648
Xavier <xpfplantaz@connect.ust.hk>
2017-12-19 22:36:21 +0800

    Add Part 2 13 - 15

--- day13.py
+++ day13.py
@@ -17,3 +17,29 @@ with open('input/day13.txt', 'r') as input_file:
 4: 4
 6: 4'''))
     print (part1(input_file.read()))
+
+
+def part2(s):
+    lines = s.split('\n')
+    total = 0
+
+    data = []
+    for line in lines:
+        depth = int(line.split(': ')[0])
+        range_ = int(line.split(': ')[1])
+        data.append({
+            'depth': depth,
+            'range_': range_
+        })
+
+    while any([(line['depth'] + total) % (2 * line['range_'] - 2) == 0 for line in data]):
+        total += 1
+
+    return total
+
+with open('input/day13.txt', 'r') as input_file:
+    print(part2('''0: 3
+1: 2
+4: 4
+6: 4'''))
+    print (part2(input_file.read()))
--- day14.py
+++ day14.py
@@ -28,13 +28,64 @@ def hex_to_bin(s):
 
 print(hex_to_bin('a0c2017'))
 
+# Part 1
+# with open('input/day14.txt') as input_file:
+#     string = input_file.read()
+#     total = 0
+#     for a in range(128):
+#         row = string + '-' + str(a)
+#         hash_ = knot_hash(row)
+#         bits = hex_to_bin(hash_)
+#         print(hash_,bits)
+#         total += sum([1 for a in bits if a == '1'])
+#     print(total)
+    
+
+# Part 2
 with open('input/day14.txt') as input_file:
     string = input_file.read()
-    total = 0
+    array = []
+
     for a in range(128):
         row = string + '-' + str(a)
         hash_ = knot_hash(row)
         bits = hex_to_bin(hash_)
-        print(hash_,bits)
-        total += sum([1 for a in bits if a == '1'])
-    print(total)
+        array.append(bits)
+        
+    # print(array)
+
+    regions = 0
+    to_explore = []
+    explored = []
+
+    for index1, row in enumerate(array):
+        for index2, value in enumerate(list(row)):
+            if (index1,index2) not in explored and value == '1':
+                print(index1,index2)
+                to_explore = [(index1,index2)]
+                while len(to_explore) > 0:
+                    explored.append((index1,index2))
+                    current = to_explore.pop()
+                    x = current[0]
+                    y = current[1]
+                    
+                    if x > 0 and array[x-1][y] == '1' and (x-1,y) not in explored:
+                        to_explore.append((x-1,y))
+
+                    if x < len(array) - 1 and array[x+1][y] == '1' and (x+1,y) not in explored:
+                        to_explore.append((x+1,y))
+
+                    if y > 0 and array[x][y-1] == '1' and (x,y-1) not in explored:
+                        to_explore.append((x,y-1))
+
+                    if y < len(array) - 1 and array[x][y+1] == '1' and (x,y+1) not in explored:
+                        to_explore.append((x,y+1))
+
+                    print(to_explore)
+
+            regions += 1
+            
+
+    print(regions)
+
+
